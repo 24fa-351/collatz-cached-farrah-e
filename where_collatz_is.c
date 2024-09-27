aae743826048d3445c23ac159b5c956ada9e8f17
Ermeje <farrah.e@csu.fullerton.edu>
2024-09-27 16:44:15 -0700

    Working on csv file - formatting and such

--- where_collatz_is.c
+++ where_collatz_is.c
@@ -3,9 +3,6 @@
 #include <time.h>
 #include "where_collatz_is.h"
 
-//#define finalCollatz (1);
-FILE *collatzInfo;
-
 int randomizingNum(int max, int min){
     srand(time(NULL));
     return rand() % (max-min+1) + min;
@@ -14,11 +11,18 @@ int randomizingNum(int max, int min){
 
 int collatzingUntilOne(int randomNum){
 int collatz = randomNum;
+
 int iteration = 0;
+FILE *collatzInfo = fopen("GSheet.csv", "w");
+fprintf(collatzInfo, "Number, Iteration, Collatz\n");
+fprintf(collatzInfo, "%d", collatz);
 while (collatz !=  1){
+    iteration ++;
+    fprintf(collatzInfo, ",%d", iteration);
     if (collatz % 2 == 0) collatz = collatz/2;
     else collatz = (3*collatz)+1;
-    iteration --;
+    fprintf(collatzInfo, ",%d\n", collatz);
 }
-return iteration;
+fclose(collatzInfo);
+return 0;
 }
