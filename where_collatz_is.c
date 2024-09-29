6ef69d16a8ca9ddc094d20ff4addca781308650c
Ermeje <farrah.e@csu.fullerton.edu>
2024-09-29 13:07:54 -0700

    improved randomization

--- where_collatz_is.c
+++ where_collatz_is.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include "where_collatz_is.h"
 
 unsigned long long int randomizingNum(int max, int min){
-    srand(time(NULL));
     return rand() % (max-min+1) + min;
-    //printf("%d is the randomized num", randomizedNum);
 }
 int collatzingUntilOne(unsigned long long int randomNum){
 unsigned long long int collatz = randomNum;
